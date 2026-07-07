2df0136ec69806c834a75df5a78ca6a198c67232
Privet67 <vsezanyato41@gmail.com>
2026-07-08 01:08:22 +0500

    Create index.jsx

---
+++ pages/index.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40, fontFamily: 'Arial' }}>
+      <h1>Система подписок</h1>
+      <p>Сайт работает!</p>
+    </div>
+  );
+}
